d83ddf3c0600dc21e228ce5be2486a261978db66
BukiOffor <ebuka2264@yahoo.com>
2025-01-22 20:01:30 +0100

    feat: fragment data

--- src/erasure_coding.rs
+++ src/erasure_coding.rs
@@ -1,27 +1,35 @@
-use std::vec;
+use std::fmt::Display;
+use std::fmt;
 
 use super::ReedSolomon;
 
+#[derive(Debug)]
+pub enum Error {
+    FragmentationError,
+}
 
+impl std::error::Error for Error {}
 
-pub fn encode_fragment(data: Vec<u8>, data_shards: usize, parity_shards: usize) {
-    let mut reed_solomon = ReedSolomon::new(data_shards, parity_shards).unwrap();
+impl Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Error::FragmentationError => write!(f, "Fragmentation error"),
+        }
+    }
+}
+
+pub fn encode_fragment(data: Vec<u8>, data_shards: usize, parity_shards: usize) -> Result<Vec<Vec<u8>>, Error> {
+     if data.len() % data_shards != 0 {
+        panic!("fragmentation error")
+    }
+    let reed_solomon = ReedSolomon::new(data_shards, parity_shards).unwrap();
     let size_per_shard = data.len() / data_shards;
     let mut shards = Vec::new();
-    let mut counter = 0;
     for i in 0..data_shards{
-        let mut bound = size_per_shard * (i + 1);
-        shards.push(data[counter..bound].to_vec());
-        counter += size_per_shard ;
+        shards.push(data[(size_per_shard * i)..size_per_shard * (i + 1)].to_vec());
     }
-
-    // let mut data = vec![
-    //     data[0..32].to_vec(),
-    //     data[32..].to_vec(),
-    //     vec![0;32],
-    //     vec![0; 32],
-    //     vec![0; 32],
-       
-    // ];
+    (0..parity_shards).for_each(|_| shards.push(vec![0; size_per_shard]));
+    reed_solomon.encode(&mut shards).map_err(|_| Error::FragmentationError)?;
+    Ok(shards)
    
 }
