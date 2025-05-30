3a4a5097214fad0ad44dc52b56246e9b8bccc99a
BukiOffor <ebuka2264@yahoo.com>
2025-05-30 14:27:08 +0100

    fix: std error on reed-solomon

--- site/index.js
+++ site/index.js
@@ -1,19 +1,31 @@
-const { Account, KeyLanguage, KeyLength, accountFromMnemonic } = require('era-js');
-
+const { 
+    Account, KeyLanguage, KeyLength, accountFromMnemonic, 
+    encodeShards, decodeShards , encodeShardsTest, decodeShardsTest
+    } = require('era-js');
 
 function main(){
     let seed = new Account(KeyLength.Words24, KeyLanguage.Spanish);
-    console.log(seed.as_mnemonic())
+    //console.log(seed.as_mnemonic())
 
     let mnemonic = accountFromMnemonic("right pave sketch blanket across oppose route shell favorite domain comfort super");
-    console.log(mnemonic.as_hex())
+    console.log(mnemonic.as_bytes())
 
-    let root_key = mnemonic.derive_root_key();
-    console.log(root_key)
+    let shards = encodeShards(mnemonic.as_bytes(), 2, 3);
+    console.log("shards: ",shards)
 
-    let path = "m/44'/60'/0'/0/0";
-    let key = mnemonic.derive_extended_key(path);
-    console.log(key)
+    
+    // recover in javascript
+    let recovered = decodeShards([null, null, null, shards[3], shards[4]],2,3);
+    console.log("recoverd: ",recovered)
+    
+    
+
+    // let root_key = mnemonic.derive_root_key();
+    // console.log(root_key)
+
+    // let path = "m/44'/60'/0'/0/0";
+    // let key = mnemonic.derive_extended_key(path);
+    // console.log(key)
 
 }
 
--- src/erasure_coding.rs
+++ src/erasure_coding.rs
@@ -1,8 +1,8 @@
+#![allow(unused_imports, unused_variables)]
 use std::fmt::Display;
 use std::fmt;
 
-
-use super::{ReedSolomon, wasm_bindgen, JsValue, to_value};
+use super::{ReedSolomon, wasm_bindgen, JsValue, to_value, Uint8Array};
 
 #[derive(Debug)]
 pub enum ErasureError {
@@ -37,8 +37,8 @@ impl Display for ErasureError {
     /// # Returns
     ///
     /// A Vec of byte arrays containing the encoded data.
-#[wasm_bindgen(js_name = encodeFragment)]
-pub fn encode_fragment(data: Vec<u8>, data_shards: usize, parity_shards: usize) -> Result<JsValue, JsValue> {
+#[wasm_bindgen(js_name = encodeShards)]
+pub fn encode_shards(data: Vec<u8>, data_shards: usize, parity_shards: usize) -> Result<JsValue, JsValue> {
      if data.len() % data_shards != 0 {
         panic!("fragmentation error")
     }
@@ -52,4 +52,44 @@ pub fn encode_fragment(data: Vec<u8>, data_shards: usize, parity_shards: usize) 
     reed_solomon.encode(&mut shards).map_err(|_| <ErasureError as Into<JsValue>>::into(ErasureError::FragmentationError))?;
     Ok(to_value(&shards)?)
    
+}
+
+    /// Decodes a Vec of byte arrays using Reed-Solomon erasure coding.
+    /// The alignment of shards in the reconstruction.
+    /// e.g if shard_1, shard_2, shard_3 is missing. 
+    /// the array should be `[null,null,null,shard_4,shard_5]`
+    ///
+    /// # Arguments
+    ///
+    /// * `shards` - The shards to reconstruct
+    /// * `data_shards` - The number of data shards the shards were encoded into
+    /// * `parity_shards` - The number of parity shards the shards were encoded into
+    ///
+    /// # Returns
+    ///
+    /// A Vec of byte arrays containing the decoded data.
+    /// If the data could not be decoded, an error is returned.
+#[wasm_bindgen(js_name = decodeShards)]
+pub fn reconstruct_shards(shards: Vec<JsValue>, data_shards: usize, parity_shards: usize) -> Result<JsValue, JsValue> {
+    let reed_solomon = ReedSolomon::new(data_shards, parity_shards).unwrap();
+    let mut shards: Vec<Option<Vec<u8>>> = shards.into_iter().map(|js_value| {
+        if js_value.is_null() || js_value.is_undefined() {
+            None
+        } else {
+            Some(Uint8Array::from(js_value).to_vec())
+        }
+    }).collect();
+    reed_solomon.reconstruct(&mut shards).map_err(|_| <ErasureError as Into<JsValue>>::into(ErasureError::FragmentationError))?;
+    let shard_refs: Vec<&[u8]> = shards.iter().map(|x| x.as_deref().unwrap()).collect();
+    reed_solomon
+        .verify(&shard_refs)
+        .map_err(|e| JsValue::from_str(&format!("Verification error: {:?}", e)))?;
+
+    // Combine only the data shards
+    let mut full_data = Vec::new();
+    for shard in shards.iter().take(data_shards) {
+        full_data.extend_from_slice(shard.as_ref().unwrap());
+    }
+    Ok(Uint8Array::from(full_data.as_slice()).into())
+
 }
